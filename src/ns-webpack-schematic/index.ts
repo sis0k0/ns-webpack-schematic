488d9399836a92d4e788516cc4154fa90f12b4d9
sis0k0 <stanimira.vlaeva@gmail.com>
2018-09-09 13:39:09 +0300

    chore: fix description of schematic

--- src/ns-webpack-schematic/index.ts
+++ src/ns-webpack-schematic/index.ts
@@ -1,9 +1,6 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function nsWebpackSchematic(options: any): Rule {
+export default function nsWebpackSchematic(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
